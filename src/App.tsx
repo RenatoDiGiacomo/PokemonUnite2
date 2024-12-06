9f2b88c38f60ccdca47e49f25b16912d8e73ab6b
Felipe de Oliveira Souza <felipec8.souzadev2024@gmail.com>
2024-12-06 11:14:35 -0300

    feat: config tailwindcss on project

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,17 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <button onClick={increment}>Increment</button>
+    <div className="h-screen flex justify-center items-center">
+      <div className="border rounded-md text-center size-[300px] flex flex-col justify-center p-4 space-y-4">
+        <h1 className="font-bold text-xl">Counter</h1>
+        <p className="text-4xl">{count}</p>
+        <button
+          className="bg-blue-500 p-2 rounded-md hover:bg-blue-300 text-white text-center"
+          onClick={increment}
+        >
+          Increment
+        </button>
+      </div>
     </div>
   )
 }
