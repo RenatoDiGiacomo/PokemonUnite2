eeefc0b5ba1c582208e1195cfecc00574a80522b
Felipe de Oliveira Souza <felipec8.souzadev2024@gmail.com>
2024-12-11 09:07:00 -0300

    feat: implement user dropdown

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,14 +1,12 @@
-import { Button } from '@istic-ui/react'
-import { useAuth } from '@shared/authentication/context'
 import { RiMenu2Fill } from 'react-icons/ri'
+
+import UserDropdown from '../UserDropdown'
 
 interface HeaderProps {
   toggle: () => void
 }
 
 function Header({ toggle }: Readonly<HeaderProps>) {
-  const { signOut } = useAuth()
-
   return (
     <header className="bg-white px-12 py-6 h-24 flex justify-between items-center border-b">
       <div className="hidden md:block"></div>
@@ -19,15 +17,7 @@ function Header({ toggle }: Readonly<HeaderProps>) {
       >
         <RiMenu2Fill />
       </button>
-      <span>
-        Usuário
-        <Button
-          onClick={signOut}
-          label="Sair"
-          variant="outline"
-          color="brand-500"
-        />
-      </span>
+      <UserDropdown />
     </header>
   )
 }
